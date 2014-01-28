ca03594d12e73afaf427f0947b813ea3e0d268a3
Lucas Stadler <mi.au@papill0n.org>
2014-01-28 16:43:13 +0100

    initial commit: beats and chords.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject quick-make-music! "0.0-SNAPSHOT"
+  :description "quick, make some music!"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.9.1"]]
+  :source-paths ["."])
